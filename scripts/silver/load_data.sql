9a2602f5b945c9c138a9b3743d16358ef82cba8d
durgeshnandani <145182943+durgeshnandani@users.noreply.github.com>
2026-09-19 16:54:55 +0530

    Update load_data.sql

--- scripts/silver/load_data.sql
+++ scripts/silver/load_data.sql
@@ -61,3 +61,46 @@ where flag_last =1
 ----See the data in silver layer
 
 select * from silver.crm_cust_info;
+
+
+---same data cleaning for crm_prd_info
+
+select *  from bronze.crm_prd_info;
+--- check for duplicates and and unwanted space 
+--- always remembber we have to split the column prd_key as it has 2 info mapping to sales details and erp_PX_CAT_G1V2
+
+select prd_id, count(*) from bronze.crm_prd_info 
+group by prd_id
+having count(*)>1 or prd_id is null; ---seems like there is no duplicates 
+ ---lets split the column, replace null with 0 for cost and check if any cost is negavite then replace it with 0
+ -- and remove abbreviation, check that start date is less than end date if not fix it , remove timestamp also 
+
+ select prd_id,replace(substring(prd_key,1,5) , '-' , '_') as cat_id ,
+ substring(prd_key,7,len(prd_key)) as prd_key , prd_nm,
+ ISNULL(prd_cost,0) as prd_cost , 
+ case 
+  when upper(TRIM(prd_line)) = 'M' THEN 'Mountain'
+  when upper(TRIM(prd_line)) = 'R' THEN 'Road'
+  when upper(TRIM(prd_line)) = 'S' THEN 'Other Sales'
+  when upper(TRIM(prd_line)) = 'T' THEN 'Touring'
+  else 'n/a' 
+end as prd_line , CAST(prd_start_dt as date) AS prd_start_dt,cast(LEAD(prd_start_dt) over(partition by prd_key order by prd_start_dt) -1 as date) as prd_end_dt
+from bronze.crm_prd_info;
+
+--- data is clean  now so insert it in the silver layer
+
+INSERT INTO silver.crm_prd_info(prd_id,cat_id,prd_key,prd_nm,prd_cost,prd_line,prd_start_dt,prd_end_dt)
+select prd_id,replace(substring(prd_key,1,5) , '-' , '_') as cat_id ,
+ substring(prd_key,7,len(prd_key)) as prd_key ,
+ prd_nm,ISNULL(prd_cost,0) as prd_cost , 
+ case 
+  when upper(TRIM(prd_line)) = 'M' THEN 'Mountain'
+  when upper(TRIM(prd_line)) = 'R' THEN 'Road'
+  when upper(TRIM(prd_line)) = 'S' THEN 'Other Sales'
+  when upper(TRIM(prd_line)) = 'T' THEN 'Touring'
+  else 'n/a' 
+end as prd_line , CAST(prd_start_dt as date) AS prd_start_dt,cast(LEAD(prd_start_dt) over(partition by prd_key order by prd_start_dt) -1 as date) as prd_end_dt
+from bronze.crm_prd_info;
+
+--check the data
+select * from silver.crm_prd_info;
